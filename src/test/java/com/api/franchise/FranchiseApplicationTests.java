c8669320588153b06784157a9008f41312e25426
kgisselledev <slayerdragonland@gmail.com>
2024-08-10 22:36:17 -0500

    First commit

---
+++ src/test/java/com/api/franchise/FranchiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.franchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
